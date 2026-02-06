182c8079d518ec2d09ff450d305327e6dd14a04b
KGriffin90 <kimanigriffin@hotmail.com>
2026-02-06 15:46:57 -0700

    Website Update

--- js/scripts.js
+++ js/scripts.js
@@ -34,16 +34,18 @@ window.addEventListener('DOMContentLoaded', event => {
     };
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
+    const navbarToggler = document.querySelector('.navbar-toggler');
+    const responsiveNavItems = document.querySelectorAll('#navbarResponsive .nav-link');
+
+    if (navbarToggler) {
+        responsiveNavItems.forEach((responsiveNavItem) => {
+            responsiveNavItem.addEventListener('click', () => {
+                const style = window.getComputedStyle(navbarToggler);
+                if (style.display !== 'none') {
+                    navbarToggler.click();
+                }
+            });
         });
-    });
+    }
 
 });
